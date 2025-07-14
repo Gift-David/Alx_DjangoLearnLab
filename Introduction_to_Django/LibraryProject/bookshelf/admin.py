a432d029ee6236d09077db549877562b3571b259
David Gift <giftdavid189@gmail.com>
2025-07-14 18:46:23 +0100

    added django-models

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -3,11 +3,9 @@ from .models import Book
 
 # Register your models here.
 
-from django.contrib import admin
-from .models import Book
-
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_filter = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
 
-admin.site.register(Book, BookAdmin)
+admin.site.register(Book)
--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+# from django.urls import reverse
 
 # Create your models here.
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    publication_year = models.IntegerField()
+    title = models.CharField(max_length=200),
+    author = models.CharField(max_length=200),
+    publication_year = models.IntegerField(),
