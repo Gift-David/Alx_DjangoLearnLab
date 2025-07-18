b2887b41a4f21b5561c36026480c9bc0101bf932
David Gift <giftdavid189@gmail.com>
2025-07-18 06:20:13 +0100

    updated auth feature

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -21,7 +21,7 @@ class ViewLibrary(DetailView):
     template_name = 'relationship_app/library_detail.html'
 
 
-class RegisterView(CreateView):
+class UserCreationForm(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
-    template_name = "registration/signup.html"
+    template_name = "relationship_app/register.html"
