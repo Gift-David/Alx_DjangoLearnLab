ad367c0ec2474b03179bebecee24449388b9c124
David Gift <giftdavid189@gmail.com>
2025-07-20 19:07:26 +0100

    updated the admin view

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,9 +15,9 @@ urlpatterns = [
     path('accounts/login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('accounts/logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('accounts/register/', UserCreationForm.as_view(template_name='relationship_app/register.html'), name='register'),
-    path('admin-dashboard/', views.admin_view, name='admin'),
-    path('librarian/', librarian_view.librarian_dashboard, name='librarian'),
-    path('member/', member_view.member_dashboard, name='member'),
+    path('admin-only/', views.admin_view, name='admin_view'),
+    path('librarian/', librarian_view.librarian_dashboard, name='librarian_view'),
+    path('member/', member_view.member_dashboard, name='member_view'),
     path('add_book/', views.add_book, name='add_book'),
     path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
     path('delete_book/<int:pk>/', views.delete_book, name='delete_book')
