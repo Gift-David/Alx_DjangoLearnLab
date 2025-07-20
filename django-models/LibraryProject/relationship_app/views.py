6bb3474b74ab648b55847333a439016207f9039e
David Gift <giftdavid189@gmail.com>
2025-07-20 15:51:52 +0100

    updated the admin view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -84,7 +84,7 @@ def is_admin(user):
     
 @login_required   
 @user_passes_test(is_admin)
-def admin_dashboard(request):
+def admin_view(request):
     return HttpResponse("Admin Dashboard")
 
 # UserCreationForm()
