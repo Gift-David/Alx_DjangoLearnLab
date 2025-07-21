90ad43d9765ba11f59f95af4c5af273c91d928fc
David Gift <giftdavid189@gmail.com>
2025-07-21 14:50:41 +0100

    updated custom manager feature

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,17 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.base_user import BaseUserManager
 # from django.urls import reverse
 
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    # email = models.EmailField(unique=True, blank=False)
     date_of_birth = models.DateField(blank=False)
     profile_photo = models.ImageField(upload_to='static/profile_pics', blank=True)
-    # USERNAME_FIELD = email
-
-    # def __str__(self):
-    #     return f"{self.username}"
     
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, date_of_birth, profile_photo, password=None):
