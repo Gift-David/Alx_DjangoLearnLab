1be71c6c8da461050800ab83bfd81f392cc4435e
David Gift <giftdavid189@gmail.com>
2025-07-22 03:04:36 +0100

    updated the models

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.contrib.auth.base_user import BaseUserManager
+from datetime import date
 # from django.urls import reverse
 
 # Create your models here.
@@ -13,8 +14,10 @@ class CustomUserManager(BaseUserManager):
     def create_user(self, email, date_of_birth, profile_photo, password=None):
         pass
 
-    def create_superuser(self, email, date_of_birth, profile_photo, password=None):
-        pass
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('date_of_birth', '2025-07-07')
+        return self.create_user(username, password, **extra_fields)
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200),
@@ -23,3 +26,11 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        permissions = [
+            ('can_view', 'Can View Book'),
+            ('can_create', 'Can Create Book'),
+            ('can_edit', 'Can Edit Book'),
+            ('can_delete', 'Can Delete Book'),
+        ]
