5c63db28ec477c8ecedaf35f7e478aab55f94925
David Gift <giftdavid189@gmail.com>
2025-08-01 10:51:14 +0100

    updated the auth and permission feature

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from rest_framework import generics, viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 from .models import Book
 from .serializers import BookSerializer
 
@@ -12,3 +15,10 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class BookAPIAuthView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return HttpResponse({'message': 'Hello auth user'})
