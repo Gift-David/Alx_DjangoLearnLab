89f350b157c33ddf882538483bc09e2d4b327837
David Gift <giftdavid189@gmail.com>
2025-08-07 12:15:07 +0100

    implimented search and filter feature

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Author, Book
 from .serializers import BookSerializer, AuthorSerializer
-from rest_framework import generics
+from rest_framework import generics, filters
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, IsAdminUser
 
@@ -12,6 +12,8 @@ class BookListView(generics.ListAPIView):
     serializer_class = BookSerializer
 
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    search_fields = ['title', 'author']
 
 class BookDetailView(generics.RetrieveAPIView):
     serializer_class = BookSerializer
