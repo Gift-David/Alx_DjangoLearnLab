dea974f8bedc9a70b786f9134cbc64838d593311
David Gift <giftdavid189@gmail.com>
2025-08-13 14:19:01 +0100

    impliment authentication and user profile features

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import RegistrationView, UserProfileView
+
+urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegistrationView.as_view(), name='register'),
+    path('profile/', UserProfileView.as_view(), name='user_profile')
+]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView, DetailView
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+class RegistrationView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = ''
+
+class UserProfileView(DetailView):
+    model = User
+    template_name = ''
