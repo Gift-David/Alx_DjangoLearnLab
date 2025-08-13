a303b33f82275f86ee4041f14e3a4bad208f9104
David Gift <giftdavid189@gmail.com>
2025-08-13 15:04:05 +0100

    add model for comment feature

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -8,3 +8,10 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateField()
+    updated_at = models.DateField()
