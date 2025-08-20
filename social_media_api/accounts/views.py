fe9d04c9e928f21e35fe36e9934eead51e57a814
David Gift <giftdavid189@gmail.com>
2025-08-20 23:27:52 +0100

    impliment endpoints

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from rest_framework import generics
+from .models import CustomUser
+from .serializers import ProfileSerializer
 
 # Create your views here.
+
+class RegistrationView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+
+class ProfileView(generics.RetrieveAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = ProfileSerializer
