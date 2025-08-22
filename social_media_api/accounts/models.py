baf0fb69ee80d9504d5c10c8eb5884aa07d7797a
David Gift <giftdavid189@gmail.com>
2025-08-22 14:28:01 +0100

    updated comment features

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -6,4 +6,19 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     bio = models.TextField()
     profile_picture = models.ImageField(upload_to='', default='')
-    followers = models.ManyToManyField('self', symmetrical=False, related_name='follower')
+    followers = models.ManyToManyField('self', symmetrical=False)
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='customuser_set',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups'
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='customuser_set',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions'
+    )
